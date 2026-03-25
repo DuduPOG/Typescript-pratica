cf0684fa8d171b077d867333929c8cd5eff79aff
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-03-25 14:21:23 -0300

    funções em typescript

--- teste.ts
+++ teste.ts
@@ -13,8 +13,8 @@ console.log(`Este é o objeto ${pessoa1.nome} - ${pessoa1.sobrenome} - ${pessoa1
 pessoa1.aniversario();
 console.log(`Idade alterada: ${pessoa1.idade}`);
 
-*/
 // const vazio = []; // tipo any, ou seja, pode-se adicionar qualquer tipo
+
 const cores = ['verde', 'azul', 'amarelo']; // tipo string, so aceita adições de strings
 
 const cores2 = ['branco', 'preto', 'vermelho'];
@@ -22,7 +22,35 @@ const cores2 = ['branco', 'preto', 'vermelho'];
 for (let cor of cores){
     console.log(cor);
 }
+*/
+function multiplicar(n1: number, n2: number) : number {
+    return n1 * n2;
+}
 
+let fatorial = function (n: number) : number {
+    if (n === 1){
+        return 1;
+    }
+    return n * fatorial(n - 1);
+}
+
+// função anônima
+let multiplicarA = function (n1 : number, n2 : number) : number {
+    return n1 * n2;
+};
+
+let resultado: number = multiplicar(10, 3);
+
+let raiz2 = (n: number) => n ** 0.5;
+
+console.log(raiz2(1));
+console.log(raiz2(4));
+console.log(raiz2(9));
+console.log(raiz2(16));
+console.log(raiz2(25));
+console.log(fatorial(5));
+console.log(resultado);
+console.log(multiplicarA(10, 3));
 /*
 let cor = cores.pop();
 
