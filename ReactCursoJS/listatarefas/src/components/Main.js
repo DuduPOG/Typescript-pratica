9eb21c70f12607f8d801b811eef66b576e942ab8
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-05-01 15:31:30 -0300

    feat: adicionando ícones padrão react e estilizando form

--- ReactCursoJS/listatarefas/src/components/Main.js
+++ ReactCursoJS/listatarefas/src/components/Main.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import './Main.css';
+// Form
+import { FaPlus } from 'react-icons/fa';
 
 export default class Main extends Component {
 
@@ -14,14 +16,21 @@ export default class Main extends Component {
   }
 
   render() {
-    //const { novaTarefa } = this.state;
+    const { novaTarefa } = this.state;
     return (
       <div className="main">
-        <h1>Lista de Tarefas</h1>
+        <h1>Lista de Tarefas {  }</h1>
 
-        <form action='#'>
-          <input type="text" onChange={this.handleChance} />
-          <button type="submit" >Salvar</button>
+        <form action='#' className="form">
+          <input
+            type="text"
+            onChange={this.handleChance}
+            value={ novaTarefa }
+          />
+          <button type="submit" >
+            <FaPlus />
+          </button>
+
         </form>
       </div>
     );
