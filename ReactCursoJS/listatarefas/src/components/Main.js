64c382e486ee23f48f7724d45aac6f60eaba345d
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-05-02 14:49:19 -0300

    feat: adicionando operações de crud sem salvamento no local Storage e estilizando elementos do crud

--- ReactCursoJS/listatarefas/src/components/Main.js
+++ ReactCursoJS/listatarefas/src/components/Main.js
@@ -3,11 +3,67 @@ import './Main.css';
 // Form
 import { FaPlus } from 'react-icons/fa';
 
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 export default class Main extends Component {
 
   state = {
     novaTarefa: '',
+    tarefas: [],
+    index: -1,
   };
+
+  handleSubmit = (evento) => {
+    evento.preventDefault();
+    //console.log("Oi");
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1){
+      return;
+    }
+    const novasTarefas = [...tarefas];
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
+  }
+
+  handleDelete = (evento, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+
+    return;
+  }
+
+  handleEdit = (evento, index) => {
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+
+  }
+
+
+
 
   handleChance = (evento) => {
     this.setState({
@@ -16,12 +72,12 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas {  }</h1>
 
-        <form action='#' className="form">
+        <form  onSubmit={ this.handleSubmit } action='#' className="form">
           <input
             type="text"
             onChange={this.handleChance}
@@ -30,8 +86,25 @@ export default class Main extends Component {
           <button type="submit" >
             <FaPlus />
           </button>
-
         </form>
+
+        <ul className="tarefas" >
+          { tarefas.map((tarefa, index) => (
+            <li key={ tarefa } >
+              { tarefa }
+            <span>
+                <FaEdit
+                  className="edit" onClick={ (evento) => this.handleEdit(evento, index) }
+                />
+
+                <FaWindowClose
+                  className="delete" onClick={ (evento) => this.handleDelete(evento, index) }
+                />
+
+            </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
